3274310a4c8011ef07279f1682980b23bb8ac775
maguangcan <184425506@qq.com>
2024-09-26 23:26:18 +0800

    static目录上传

--- runtime/dev/temp/51e0d0cfec3c2e36172c06bfa7e51207.php
+++ runtime/dev/temp/51e0d0cfec3c2e36172c06bfa7e51207.php
@@ -1,4 +1,4 @@
-<?php /*a:1:{s:84:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/index/index.vue";i:1727281681;}*/ ?>
+<?php /*a:1:{s:84:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/index/index.vue";i:1727360013;}*/ ?>
 <template>
   <div class="name">哈哈哈哈哈 这是一个也没{{ msg }}</div>
 </template>
--- runtime/dev/temp/7235e0c5c8fbac1d674f7fecf6017fad.php
+++ runtime/dev/temp/7235e0c5c8fbac1d674f7fecf6017fad.php
@@ -1,4 +1,4 @@
-<?php /*a:3:{s:87:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/_layout/index.html";i:1727281069;s:86:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/_public/_CSS.html";i:1726764677;s:85:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/_public/_JS.html";i:1727281237;}*/ ?>
+<?php /*a:4:{s:87:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/_layout/index.html";i:1727364298;s:86:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/_public/_CSS.html";i:1726764677;s:85:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/_public/_JS.html";i:1727364310;s:89:"/Users/sunflower816/PhpstormProjects/fast_admin_vue_tp6/app/dev/view/_layout/default.html";i:1727363461;}*/ ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -26,7 +26,9 @@
 <script type="text/javascript" src="/dev/plugins/vue/vue.min.js"></script>
 
 <!--HttpVueLoader-->
+<script type="text/javascript" src="/dev/plugins/vue/sass.js"></script>
 <script type="text/javascript" src="/dev/plugins/vue/httpVueLoader.js"></script>
+
 <!--axios-->
 <script type="text/javascript" src="/dev/plugins/axios/axios.min.js"></script>
 <script type="text/javascript" src="/dev/js/axios-base.js"></script>
@@ -40,12 +42,21 @@
   </head>
   <body>
     <!--内容前的初始化-->
-    <script >
+    <script>
       const allComponents = JSON.parse(`<?php echo json_encode(initVueComponents()); ?>`);
 
       initVue2();
 
       function initVue2() {
+        // 支持scss
+        httpVueLoader.langProcessor.scss = function (scssText) {
+          return new Promise(function (resolve, reject) {
+            sass.compile(scssText, function (result) {
+              if (result.status === 0) resolve(result.text);
+              else reject(result);
+            });
+          });
+        };
         //vue的跨组件引用
         Vue.use(httpVueLoader);
         Vue.prototype.request = $axios;
@@ -59,58 +70,143 @@
         }
       }
     </script>
-    <main>
-      <!--    模版内容-->
+    <!--    模版内容-->
+    <div id="app">
+  <el-container class="app-el-container" direction="vertical">
+    <el-header class="el-header" height="">
+      <!-- Header content -->
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+    <el-container class="app-body" direction="horizontal">
+      <el-aside class="app-el-aside" width="200px">
+        <!-- Aside content -->
+        <el-menu :default-openeds="['1', '3']">
+          <el-submenu index="1">
+            <template slot="title"
+              ><i class="el-icon-message"></i>导航一</template
+            >
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="2-1">选项1</el-menu-item>
+              <el-menu-item index="2-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="2-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="2-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"
+              ><i class="el-icon-setting"></i>导航三</template
+            >
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="3-1">选项1</el-menu-item>
+              <el-menu-item index="3-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="3-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="3-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="3-4-1">选项4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container direction="vertical">
+        <el-main height="">
+          <!-- Main content -->
+          <app></app>
+        </el-main>
+        <el-footer height="">
+          <!-- Footer content -->
+        </el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+</div>
 
-      <div id="app">
-        <div class="el-card" style="margin: 20px" @click="onAdd()">
-          测试页面 {{msg}} 你好啊 <?php echo htmlentities($__APP_URL__); ?>
+<script>
+  const app = new Vue({
+    el: "#app",
+    components: {
+      app: `url:<?php echo htmlentities($__APP_URL__); ?>`,
+    },
+    data() {
+      return {
+        msg: 1,
+      };
+    },
+    methods: {
+      onAdd() {
+        this.msg++;
+      },
+    },
+  });
+</script>
 
-          <div class="name">1111</div>
-        </div>
-        <vue-label :title="msg"></vue-label>
-        <!-- 应用页面 -->
-        <app></app>
-      </div>
+<style scoped>
+  html {
+    height: 100vh;
+  }
+  body {
+    background: #f6f8f9;
+    margin: 0px;
+    height: 100%;
+  }
 
-      <script>
-        const app = new Vue({
-          el: "#app",
-          components: {
-            app: `url:<?php echo htmlentities($__APP_URL__); ?>`,
-          },
-          data() {
-            return {
-              msg: 1,
-            };
-          },
-          methods: {
-            onAdd() {
-              this.msg++;
-            },
-          },
-        });
-      </script>
+  #app {
+    height: 100%;
+  }
 
-      <style scoped>
-        body {
-          background: #f6f8f9;
-          margin: 0px;
-        }
+  .app-el-container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 
-        .main-body {
-          width: 100%;
-          height: calc(100vh - 100px);
-          position: relative;
-        }
+  .app-body {
+    height: calc(100% - 60px);
+  }
 
-        .iframe-component {
-          width: 100%;
-          height: 100%;
-          overflow: auto;
-          border: none;
-        }
-      </style>
-    </main>
+  .el-header {
+    background-color: #fff;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .app-el-aside {
+    color: #333;
+    background-color: #f5f5f5;
+  }
+</style>
+
   </body>
 </html>
