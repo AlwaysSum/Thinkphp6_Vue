05d2846f2b4cba22d44703e53ab871dccf2e4214
maguangcan <184425506@qq.com>
2024-10-03 06:09:37 +0800

    feat:控制台

--- app/admin/view/_components/index/header.vue
+++ app/admin/view/_components/index/header.vue
@@ -14,6 +14,7 @@
         src="https://www.tencent.com/img/index/menu_logo_hover.png"
         alt="logo"
       />
+
       <t-menu-item name="item1">菜单内容一</t-menu-item>
       <t-menu-item name="item2">已选内容</t-menu-item>
       <t-menu-item name="item3">菜单内容二</t-menu-item>
--- app/admin/view/dashboard/index.vue
+++ app/admin/view/dashboard/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="dashboard-page">
+    <!-- 顶部Card -->
     <t-row :gutter="[16, 16]">
       <t-col
         :xs="6"
@@ -65,6 +66,44 @@
         </t-card>
       </t-col>
     </t-row>
+
+    <!-- 排名 -->
+    <t-row :gutter="[16, 16]">
+      <t-col :xs="12" :xl="6">
+        <t-card
+          title="销售订单排名"
+          class="dashboard-rank-card"
+          :bordered="false"
+        >
+          <template #actions>
+            <t-radio-group default-value="dateVal">
+              <t-radio-button value="dateVal">本周</t-radio-button>
+              <t-radio-button value="monthVal">近三个月</t-radio-button>
+            </t-radio-group>
+          </template>
+          <t-table
+            :data="saleTendList"
+            :columns="saleColumns"
+            rowKey="productName"
+          >
+            <template #index="{ rowIndex }">
+              <span :class="getRankClass(rowIndex)">
+                {{ rowIndex + 1 }}
+              </span>
+            </template>
+            <span slot="growUp" slot-scope="{ row }">
+              <trend
+                :type="row.growUp > 0 ? 'up' : 'down'"
+                :describe="Math.abs(row.growUp)"
+              />
+            </span>
+            <template #operation="slotProps">
+              <a class="link" @click="rehandleClickOp(slotProps)">详情</a>
+            </template>
+          </t-table>
+        </t-card>
+      </t-col>
+    </t-row>
   </div>
 </template>
 
@@ -99,7 +138,97 @@ module.exports = {
           leftType: "icon-file-paste",
         },
       ],
+      saleTendList: [
+        {
+          growUp: 1,
+          productName: "国家电网有限公司",
+          count: 7059,
+          date: "2021-09-01",
+        },
+        {
+          growUp: -1,
+          productName: "深圳燃气集团股份有限公司",
+          count: 6437,
+          date: "2021-09-01",
+        },
+        {
+          growUp: 4,
+          productName: "国家烟草专卖局",
+          count: 4221,
+          date: "2021-09-01",
+        },
+        {
+          growUp: 3,
+          productName: "中国电信集团有限公司",
+          count: 3317,
+          date: "2021-09-01",
+        },
+        {
+          growUp: -3,
+          productName: "中国移动通信集团有限公司",
+          count: 3015,
+          date: "2021-09-01",
+        },
+        {
+          growUp: -3,
+          productName: "新余市办公用户采购项目",
+          count: 2015,
+          date: "2021-09-12",
+        },
+      ],
+      saleColumns: [
+        {
+          align: "center",
+          colKey: "index",
+          title: "排名",
+          width: 80,
+          fixed: "left",
+        },
+        {
+          align: "left",
+          ellipsis: true,
+          colKey: "productName",
+          title: "客户名称",
+          minWidth: 200,
+        },
+        {
+          align: "center",
+          colKey: "growUp",
+          width: 100,
+          title: "较上周",
+        },
+        {
+          align: "center",
+          colKey: "count",
+          title: "订单量",
+          width: 100,
+        },
+        {
+          align: "center",
+          colKey: "date",
+          width: 140,
+          title: "合同签订日期",
+        },
+        {
+          align: "center",
+          colKey: "operation",
+          title: "操作",
+          width: 80,
+          fixed: "right",
+        },
+      ],
     };
+  },
+  methods: {
+    rehandleClickOp(val) {
+      console.log(val);
+    },
+    getRankClass(index) {
+      return [
+        "dashboard-rank__cell",
+        { "dashboard-rank__cell--top": index < 3 },
+      ];
+    },
   },
 };
 </script>
@@ -107,6 +236,9 @@ module.exports = {
 <style scoped>
 .dashboard-page {
   padding: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 }
 
 .row-container {
@@ -199,5 +331,32 @@ module.exports = {
 }
 .dashboard-item--main-color .dashboard-item-bottom {
   color: var(--td-text-color-anti);
+}
+
+/** 公共前缀 */
+.dashboard-rank-card {
+  padding: 8px;
+}
+.dashboard-rank-card .t-card__header {
+  padding-bottom: 24px;
+}
+.dashboard-rank-card .t-card__title {
+  font-size: 20px;
+  font-weight: 500;
+}
+.dashboard-rank__cell {
+  display: inline-flex;
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  color: white;
+  font-size: 14px;
+  background-color: var(--td-gray-color-5);
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+}
+.dashboard-rank__cell--top {
+  background: var(--td-brand-color);
 }
 </style>
